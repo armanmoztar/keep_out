a0b5e12d275a6ddeebed448887810656a730d041
Arman Moztarzadeh <armanmoztar@outlook.com>
2022-05-15 01:07:47 -0700

    added twilio code

---
+++ send-sms.js
@@ -0,0 +1,12 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+    to: '+17789536129',
+    from: '+18508483621',
+    body: "Test"
+})
+// logout message ID when message is sent
+.then(message => console.log(message.sid));
