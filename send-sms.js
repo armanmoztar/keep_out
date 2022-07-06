b561f57a5849c074ddfea0e7f528cbe32ef18abe
samanthatuhseng <samantha.tseng90@gmail.com>
2022-07-05 19:27:48 -0700

    flask server and videocam on site

--- send-sms.js
+++ send-sms.js
@@ -1,16 +1,17 @@
-require('dotenv').config();
+// require('dotenv').config();
 // console.log('Your environment variable TWILIO_ACCOUNT_SID has the value: ', process.env.TWILIO_ACCOUNT_SID);
 // console.log('Your authentication variable TWILIO_AUTH_TOKEN has the value: ', process.env.TWILIO_AUTH_TOKEN);
 
 const accountSid = "ACb7a6cc0c91a6d8d33dc14fd31e110206"
-const authToken = "2431774bc9a084891f1b3b04b2d250d4"
+const authToken = "25c30b7d8edd00f704ec8a558accd9a4"
 
 const client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
     to: '+17789536129',
-    from: '+1778743-3256',
-    body: "Keep-out: Please type 'Yes' to confirm your phone number"
+    from: '+17787433256',
+    statusCallback: 'https://eoniira9qziky3r.m.pipedream.net',
+    body: "Keep-out: Please reply with anything to confirm your phone number. Otherwise, ignore this message"
 })
 // logout message ID when message is sent
 .then(message => console.log(message.sid));
